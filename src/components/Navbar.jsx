0641b5c74f9ae379f39d65b394f315e25f3e3ed3
izzat <izzatfadzlon@gmail.com>
2024-02-12 23:01:33 +0800

    Lesson 7:Card Component

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -11,7 +11,7 @@ import React from "react";
 
 export default function Navbar() {
   return (
-    <Flex as="nav" p="10px" alignItems="center">
+    <Flex as="nav" p="10px" alignItems="center" mb="40px">
       <Heading as="h1">Dojo Tasks</Heading>
       <Spacer />
 
--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,24 +1,78 @@
-import { Box, SimpleGrid, Text } from "@chakra-ui/react";
+import { EditIcon, ViewIcon } from "@chakra-ui/icons";
+import {
+  Box,
+  Button,
+  Card,
+  CardBody,
+  CardFooter,
+  CardHeader,
+  Divider,
+  Flex,
+  HStack,
+  Heading,
+  ListIcon,
+  SimpleGrid,
+  Text,
+} from "@chakra-ui/react";
+import axios from "axios";
+import { useEffect, useState } from "react";
+// import { myTasks } from "../../data/db";
 
 export default function Dashboard() {
+  const [tasks, setTasks] = useState([]);
+
+  const tasksLoader = async () => {
+    try {
+      const res = await axios.get("http://localhost:3000/tasks");
+      setTasks(res.data);
+      console.log(res.data);
+    } catch (error) {
+      console.log("error encountered: ", error);
+    }
+  };
+
+  useEffect(() => tasksLoader, []);
+
   return (
     <SimpleGrid spacing={10} minChildWidth="250px">
-      <Box bg="white" h="200px" border="1px">
+      {tasks &&
+        tasks.map((task) => (
+          <Card
+            key={task.id}
+            borderTop={"8px"}
+            borderColor={"purple.400"}
+            bg={"white"}
+          >
+            <CardHeader>
+              <Flex gap="5px">
+                <Box w="50px" h="50px">
+                  AV
+                </Box>
+                <Box>
+                  <Heading as="h3" size="small">
+                    {task.title}
+                  </Heading>
+                  <Text>by {task.author}</Text>
+                </Box>
+              </Flex>
+            </CardHeader>
+            <CardBody>{task.description}</CardBody>
+            <Divider borderColor="gray.200" />
+            <CardFooter>
+              <HStack>
+                <Button variant="ghost" leftIcon={<ViewIcon />}>
+                  Watch
+                </Button>
+                <Button variant="ghost" leftIcon={<EditIcon />}>
+                  Comment
+                </Button>
+              </HStack>
+            </CardFooter>
+          </Card>
+        ))}
+      {/* <Box bg="white" h="200px" border="1px">
         <Text color={{ base: "black", sm: "orange", md: "green" }}>hello</Text>
-      </Box>
-      <Box bg="white" h="200px" border="1px"></Box>
-      <Box bg="white" h="200px" border="1px"></Box>
-      <Box bg="white" h="200px" border="1px"></Box>
-
-      <Box bg="white" h="200px" border="1px"></Box>
-      <Box bg="white" h="200px" border="1px"></Box>
-      <Box bg="white" h="200px" border="1px"></Box>
-      <Box bg="white" h="200px" border="1px"></Box>
-
-      <Box bg="white" h="200px" border="1px"></Box>
-      <Box bg="white" h="200px" border="1px"></Box>
-      <Box bg="white" h="200px" border="1px"></Box>
-      <Box bg="white" h="200px" border="1px"></Box>
+      </Box> */}
     </SimpleGrid>
 
     // {*Lesson 2*}
